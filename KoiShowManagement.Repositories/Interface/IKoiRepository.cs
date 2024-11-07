ac60a63bce71fc525f85a80dbd31b39b8a9ac0be
Datbt <datbuithanh1711@gmail.com>
2024-11-07 23:16:44 +0700

    đạt

---
+++ KoiShowManagement.Repositories/Interface/IKoiRepository.cs
@@ -0,0 +1,14 @@
+﻿using KoiShowManagementSystem.Repositories.Entities;
+
+namespace KoiShowManagement.Repositories.Interface
+{
+    public interface IKoiRepository
+    {
+        Task<List<Koi>> GetAllKois();
+        bool DelKoi(int Id);
+        bool DelKoi(Koi koi);
+        bool AddKoi(Koi koi);
+        bool UpdKoi(Koi koi);
+        Task<Koi> GetKoiById(int Id);
+    }
+}
