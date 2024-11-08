2a03ed687f4db7849a66602d8a8b9219f75bcc6d
Datbt <datbuithanh1711@gmail.com>
2024-11-08 14:04:14 +0700

    đạt

---
+++ KoiShowManagement.Repositories/Interface/IGuestRepository.cs
@@ -0,0 +1,15 @@
+﻿using KoiShowManagement.Repositories.Entities;
+
+
+namespace KoiShowManagement.Repositories.Interface
+{
+    public interface IGuestRepository
+    {
+        Task<List<Guest>> GetAllGuests();
+        bool DelGuest(int Id);
+        bool DelGuest(Guest guest);
+        bool AddGuest(Guest guest);
+        bool UpdGuest(Guest guest);
+        Task<Guest> GetGuestById(int Id);
+    }
+}
