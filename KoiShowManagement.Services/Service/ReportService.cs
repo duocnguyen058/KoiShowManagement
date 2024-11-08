f2bc4f13781587e99e0aec37a9b93b884ff5c018
HauDTg <tronghau105@gmail.com>
2024-11-09 00:16:57 +0700

    Add files via upload

---
+++ KoiShowManagement.Services/Service/ReportService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Repositories.Interface;
+using KoiShowManagement.Repositories.Repository;
+using KoiShowManagement.Services.Interface;
+
+namespace KoiShowManagement.Services.Service
+{
+    public class ReportService : IReportService
+    {
+        private readonly IReportRepository _repository;
+        public ReportService(IReportRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool AddReport(Report report)
+        {
+            return _repository.AddReport(report);
+        }
+
+        public bool DelReport(int Id)
+        {
+            return _repository.DelReport(Id);
+        }
+
+        public bool DelReport(Report report)
+        {
+            return _repository.DelReport(report);
+        }
+
+        public Task<List<Report>> GetAllReports()
+        {
+            return _repository.GetAllReports();
+        }
+
+        public Task<Report> GetReportById(int Id)
+        {
+            return _repository.GetReportById(Id);
+        }
+
+        public bool UpdReport(Report report)
+        {
+            return _repository.UpdReport(report);
+        }
+    }
+}
---
+++ KoiShowManagement.Services/Service/UserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Repositories.Interface;
+using KoiShowManagement.Services.Interface;
+
+namespace KoiShowManagement.Services.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool AddUser(User user)
+        {
+            return _repository.AddUser(user);
+        }
+
+        public bool DelUser(int Id)
+        {
+            return _repository.DelUser(Id);
+        }
+
+        public bool DelUser(User user)
+        {
+            return _repository.DelUser(user);
+        }
+
+        public Task<List<User>> GetAllUsers()
+        {
+            return _repository.GetAllUsers();
+        }
+
+        public Task<User> GetUserById(int Id)
+        {
+            return _repository.GetUserById(Id);
+        }
+
+        public bool UpdUser(User user)
+        {
+            return _repository.UpdUser(user);
+        }
+    }
+}
