dd7c9470f8639170d32bf782c39b62be3195df97
nguyenletuankiet205 <nguyenletuankiet.qng@gmail.com>
2024-11-08 17:02:59 +0700

    test

---
+++ KoiShowManagement.Repositories/Interface/IMemberRepository.cs
@@ -0,0 +1,23 @@
+﻿using KoiShowManagement.Repositories.Entities;
+using System.Collections.Generic;
+
+namespace KoiShowManagement.Repositories.Interface
+{
+    public interface IMemberRepository
+    {
+        // Thêm một thành viên mới
+        void AddMember(Member member);
+
+        // Lấy thông tin thành viên theo ID
+        Member GetMemberById(int id);
+
+        // Lấy danh sách tất cả các thành viên
+        IEnumerable<Member> GetAllMembers();
+
+        // Cập nhật thông tin thành viên
+        void UpdateMember(Member member);
+
+        // Xóa thành viên theo ID
+        void DeleteMember(int id);
+    }
+}
