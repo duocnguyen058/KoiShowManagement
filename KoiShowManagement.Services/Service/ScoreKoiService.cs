04a3b77487a8d1b527b1875dcba2addc853a3a14
Datbt <datbuithanh1711@gmail.com>
2024-11-08 22:04:10 +0700

    đạt

---
+++ KoiShowManagement.Services/Service/ScoreKoiService.cs
@@ -0,0 +1,45 @@
+﻿using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Repositories.Interface;
+using KoiShowManagement.Services.Interface;
+using System;
+namespace KoiShowManagement.Services.Service
+{
+    public class ScoreKoiService : IScoreKoiService
+    {
+        private readonly IScoreKoiRepository _repository;
+
+        public ScoreKoiService(IScoreKoiRepository repository)
+        {
+            _repository = repository;
+        }
+        public bool AddScoreKoi(ScoreKoi scoreKoi)
+        {
+            return _repository.AddScoreKoi(scoreKoi);
+        }
+
+        public bool DelScoreKoi(int Id)
+        {
+            return _repository.DelScoreKoi(Id);
+        }
+
+        public bool DelScoreKoi(ScoreKoi scoreKoi)
+        {
+            return _repository.DelScoreKoi(scoreKoi);
+        }
+
+        public Task<List<ScoreKoi>> GetAllScoreKois()
+        {
+            return _repository.GetAllScoreKois();
+        }
+
+        public Task<ScoreKoi> GetScoreKoiId(int Id)
+        {
+            return _repository.GetScoreKoiById(Id);
+        }
+
+        public bool UpdScoreKoi(ScoreKoi scoreKoi)
+        {
+            return _repository.UpdScoreKoi(scoreKoi);
+        }
+    }
+}      
