e28540683183f9efff257e5c578ed1d7fc48d1fa
nguyenletuankiet205 <nguyenletuankiet.qng@gmail.com>
2024-11-08 18:54:55 +0700

    kiet

---
+++ KoiShowManagement.Services/Service/MemberService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Repositories.Interface;
+using KoiShowManagement.Services.Interface;
+
+namespace KoiShowManagement.Services.Service
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IMemberRepository _repository;
+        public MemberService(IMemberRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public bool AddMember(Member member)
+        {
+            return _repository.AddMember(member);
+        }
+
+        public bool DelMember(int Id)
+        {
+            return _repository.DelMember(Id);
+        }
+
+        public bool DelMember(Member member)
+        {
+            return _repository.DelMember(member);
+        }
+
+        public Task<List<Member>> GetAllMembers()
+        {
+            return _repository.GetAllMember();
+        }
+
+        public Task<Member> GetMemberById(int Id)
+        {
+            return _repository.GetMemberById(Id);
+        }
+
+        public bool UpdMember(Member member)
+        {
+            return _repository.UpdMember(member);
+        }
+    }
+}
