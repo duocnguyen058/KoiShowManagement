260cce686eae361b02d2dbf055868de4ec2880e2
thangsvj <luongthangnguyen173@gmail.com>
2024-11-08 23:38:01 +0700

    thang

---
+++ KoiShowManagement.Services/Interface/IManagerService.cs
@@ -0,0 +1,16 @@
+﻿using KoiShowManagement.Repositories.Entities;
+using System;
+
+namespace KoiShowManagement.Services.Interface
+{
+    public interface IManagerService
+    {
+        Task<List<Manager>> GetAllManagers();
+        Boolean DelManager(int Id);
+        Boolean DelManager(Manager manager);
+        Boolean AddManager(Manager manager);
+        Boolean UpdManager(Manager manager);
+        Task<Guest> GetManagerById(int Id);
+        Task<Manager> GetAllManagerById(int id);
+    }
+}
