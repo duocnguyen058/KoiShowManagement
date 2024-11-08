61c84dda873bdd22c5431f10e98c2df400ffe939
Datbt <datbuithanh1711@gmail.com>
2024-11-08 22:03:36 +0700

    đạt

---
+++ KoiShowManagement.Services/Interface/IScoreKoiService.cs
@@ -0,0 +1,14 @@
+﻿using KoiShowManagement.Repositories.Entities;
+using System;
+namespace KoiShowManagement.Services.Interface
+{
+    public interface IScoreKoiService
+    {
+        Task<List<ScoreKoi>> GetAllScoreKois();
+        Boolean DelScoreKoi(int Id);
+        Boolean DelScoreKoi(ScoreKoi scoreKoi);
+        Boolean AddScoreKoi(ScoreKoi scoreKoi);
+        Boolean UpdScoreKoi(ScoreKoi scoreKoi);
+        Task<ScoreKoi> GetScoreKoiId(int Id);
+    }
+}
