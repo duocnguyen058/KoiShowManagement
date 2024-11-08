79556d2cefad4f0449e6647f7de085e47e79827d
HauDTg <tronghau105@gmail.com>
2024-11-09 00:14:59 +0700

    Add files via upload

---
+++ KoiShowManagement.Services/Interface/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using KoiShowManagement.Repositories.Entities;
+
+namespace KoiShowManagement.Services.Interface
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllUsers();
+        Boolean DelUser(int Id);
+        Boolean DelUser(User user);
+        Boolean AddUser(User user);
+        Boolean UpdUser(User user);
+        Task<User> GetUserById(int Id);
+    }
+}
