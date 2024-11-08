c9335c7c2d7507855f81d530d70ea90f1209c3b5
Datbt <datbuithanh1711@gmail.com>
2024-11-08 16:14:45 +0700

    đạt

---
+++ KoiShowManagement.Services/Service/KoiService.cs
@@ -0,0 +1,44 @@
+﻿using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Services.Interface;
+using KoiShowManagement.Repositories.Interface;
+using System;
+namespace KoiShowManagement.Services.Service
+{
+    public class KoiService : IKoiService
+    {
+        private readonly IKoiService _repository;
+        public KoiService(IKoiService repository)
+        {
+            _repository = repository;
+        }
+        public bool AddKoi(Koi koi)
+        {
+            return _repository.AddKoi(koi);
+        }
+
+        public bool DelKoi(int Id)
+        {
+            return _repository.DelKoi(Id);
+        }
+
+        public bool DelKoi(Koi koi)
+        {
+            return _repository.DelKoi(koi);
+        }
+
+        public Task<List<Koi>> GetAllKois()
+        {
+            return _repository.GetAllKois();
+        }
+
+        public Task<Koi> GetKoiById(int Id)
+        {
+            return _repository.GetKoiById(Id);
+        }
+
+        public bool UpdKoi(Koi koi)
+        {
+            return _repository.UpdKoi(koi);
+        }
+    }
+}
