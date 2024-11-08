81d960e22c63f2857f3121047e8cd17e0065a336
thangsvj <luongthangnguyen173@gmail.com>
2024-11-08 23:38:49 +0700

    thang

---
+++ KoiShowManagement.Services/Interface/IRefereeService.cs
@@ -0,0 +1,16 @@
+﻿using KoiShowManagement.Repositories.Entities;
+using System;
+
+
+namespace KoiShowManagement.Services.Interface
+{
+    public interface IRefereeService
+    {
+        Task<List<Referee>> GetAllReferees();
+        Boolean DelReferee(int Id);
+        Boolean DelReferee(Referee referee);
+        Boolean AddReferee(Referee referee);
+        Boolean UpdReferee(Referee referee);
+        Task<Referee> GetRefereeById(int Id);
+    }
+}
