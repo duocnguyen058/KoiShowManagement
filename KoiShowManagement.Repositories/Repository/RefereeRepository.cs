bbf5d5a54ae816a2f8ec89b10f271fefca73bd6a
thangsvj <luongthangnguyen173@gmail.com>
2024-11-08 23:35:43 +0700

    thang

--- KoiShowManagement.Repositories/Repository/RefereeRepository.cs
+++ KoiShowManagement.Repositories/Repository/RefereeRepository.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using KoiShowManagement.Repositories.Entities;
+﻿using KoiShowManagement.Repositories.Entities;
 using KoiShowManagement.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace KoiShowManagement.Repositories.Repository
 {
-    internal class RefereeRepository : IRefereeRepository
+    public class RefereeRepository : IRefereeRepository
     {
         private readonly KoiShowManagementDbContext _dbContext;
         public RefereeRepository(KoiShowManagementDbContext dbContext)
         {
             _dbContext = dbContext;
         }
+
         public bool AddReferee(Referee referee)
         {
             try
@@ -22,20 +23,20 @@ namespace KoiShowManagement.Repositories.Repository
             }
             catch (Exception ex)
             {
-
-                throw new NotImplementedException();
+                Console.WriteLine(ex.Message);
                 return false;
             }
         }
 
         public bool DelReferee(int Id)
         {
+
             try
             {
-                var objDel = _dbContext.Referees.Where(p => p.RefereeId.Equals(Id)).FirstOrDefault();
-                if (objDel != null)
+                var refereeToDelete = _dbContext.Referees.Where(r => r.RefereeId.Equals(Id)).FirstOrDefault();
+                if (refereeToDelete != null)
                 {
-                    _dbContext.Referees.Remove(objDel);
+                    _dbContext.Referees.Remove(refereeToDelete);
                     _dbContext.SaveChanges();
                     return true;
                 }
@@ -43,7 +44,7 @@ namespace KoiShowManagement.Repositories.Repository
             }
             catch (Exception ex)
             {
-                throw new NotImplementedException();
+                Console.WriteLine(ex.Message);
                 return false;
             }
         }
@@ -58,20 +59,19 @@ namespace KoiShowManagement.Repositories.Repository
             }
             catch (Exception ex)
             {
-
-                throw new NotImplementedException();
+                Console.WriteLine(ex.Message);
                 return false;
             }
-
-        }
-        public async Task<Referee> RefereeById(int Id)
-        {
-            return await _dbContext.Referees.Where(p => p.RefereeId.Equals(Id)).FirstOrDefaultAsync();
         }
 
         public async Task<List<Referee>> GetAllReferees()
         {
             return await _dbContext.Referees.ToListAsync();
+        }
+
+        public async Task<Referee> GetByRefereeId(int Id)
+        {
+            return await _dbContext.Referees.Where(r => r.RefereeId.Equals(Id)).FirstOrDefaultAsync();
         }
 
         public bool UpdReferee(Referee referee)
@@ -84,19 +84,9 @@ namespace KoiShowManagement.Repositories.Repository
             }
             catch (Exception ex)
             {
-                throw new NotImplementedException();
+                Console.WriteLine(ex.Message);
                 return false;
             }
         }
-        public Task<List<Referee>> GetAllReferee()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Referee> GetRefereeById(int Id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
-
