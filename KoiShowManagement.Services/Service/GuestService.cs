172730ed702f6d7d0c15a09b288d14b9fd431822
Datbt <datbuithanh1711@gmail.com>
2024-11-08 16:14:12 +0700

    đạt

---
+++ KoiShowManagement.Services/Service/GuestService.cs
@@ -0,0 +1,44 @@
+﻿using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Repositories.Interface;
+using KoiShowManagement.Services.Interface;
+using System;
+namespace KoiShowManagement.Services.Service
+{
+    public class GuestService : IGuestService
+    {
+        private readonly IGuestService _repository;
+        public GuestService(IGuestService repository)
+        {
+            _repository = repository;
+        }
+        public bool AddGuest(Guest guest)
+        {
+            return _repository.AddGuest(guest);
+        }
+
+        public bool DelGuest(int Id)
+        {
+            return _repository.DelGuest(Id);
+        }
+
+        public bool DelGuest(Guest guest)
+        {
+            return _repository.DelGuest(guest);
+        }
+
+        public Task<List<Guest>> GetAllGuests()
+        {
+            return _repository.GetAllGuests();
+        }
+
+        public Task<Guest> GetGuestById(int Id)
+        {
+            return _repository.GetGuestById(Id);
+        }
+
+        public bool UpdGuest(Guest guest)
+        {
+            return _repository.UpdGuest(guest);
+        }
+    }
+}
