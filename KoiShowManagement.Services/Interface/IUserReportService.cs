ba08bb9d35d2203d3e4bb3f95fd97c533737df69
duocnguyen058 <dcnguyn058@gmail.com>
2024-11-08 16:11:45 +0700

    duoc

---
+++ KoiShowManagement.Services/Interface/IUserReportService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using KoiShowManagement.Repositories.Entities;
+
+namespace KoiShowManagement.Services.Interface
+{
+	public interface IUserReportService
+	{
+        Task<List<UserReport>> GetAllUserReports();
+        Boolean DelUserReport(int Id);
+        Boolean DelUserReport(UserReport userReport);
+        Boolean AddUserReport(UserReport userReport);
+        Boolean UpdUserReport(UserReport userReport);
+        Task<UserReport> GetUserReportById(int Id);
+    }
+}
+
---
+++ KoiShowManagement.Services/Service/UserReportService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Repositories.Interface;
+using KoiShowManagement.Services.Interface;
+
+namespace KoiShowManagement.Services.Service
+{
+	public class UserReportService : IUserReportService
+    {
+        private readonly IUserReportRepository _repository;
+        public UserReportService(IUserReportRepository repository)
+		{
+            _repository = repository;
+		}
+
+        public bool AddUserReport(UserReport userReport)
+        {
+            return _repository.AddUserReport(userReport);
+        }
+
+        public bool DelUserReport(int Id)
+        {
+            return _repository.DelUserReport(Id);
+        }
+
+        public bool DelUserReport(UserReport userReport)
+        {
+            return _repository.DelUserReport(userReport);
+        }
+
+        public Task<List<UserReport>> GetAllUserReports()
+        {
+            return _repository.GetAllUserReport();
+        }
+
+        public Task<UserReport> GetUserReportById(int Id)
+        {
+            return _repository.GetUserReportById(Id);
+        }
+
+        public bool UpdUserReport(UserReport userReport)
+        {
+            return _repository.UpdUserReport(userReport);
+        }
+    }
+}
+
