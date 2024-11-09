2da4449778069981c8d6bcfcd7235be48362f62b
duocnguyen058 <dcnguyn058@gmail.com>
2024-11-09 09:03:26 +0700

    Update Program.cs

--- KoiShowManagement.WebApp/Program.cs
+++ KoiShowManagement.WebApp/Program.cs
@@ -1,7 +1,51 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using KoiShowManagement.Repositories.Entities;
+using KoiShowManagement.Repositories.Interface;
+using KoiShowManagement.Repositories.Repository;
+using KoiShowManagement.Services.Interface;
+using KoiShowManagement.Services.Service;
+using KoiShowManagementSystem.Repositories.Repositories;
+using KoiShowManagementSystem.Repositories.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+//DI
+builder.Services.AddDbContext<KoiShowManagementDbContext>();
+//DI Repositories
+builder.Services.AddScoped<ICompetitionResultRepository, CompetitionResultRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IGuestRepository, GuestRepository>();
+builder.Services.AddScoped<IKoiRepository, KoiRepository>();
+builder.Services.AddScoped<IManagerRepository, ManagerRepository>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
+builder.Services.AddScoped<IRefereeRepository, RefereeRepository>();
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+builder.Services.AddScoped<IScoreKoiRepository, ScoreKoiRepository>();
+builder.Services.AddScoped<IStaffRepository, StaffRepository>();
+builder.Services.AddScoped<IUserReportRepository, UserReportRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+//DI Services
+builder.Services.AddScoped<ICompetitionResultService, CompetitionResultService>();
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IGuestService, GuestService>();
+builder.Services.AddScoped<IKoiService, KoiService>();
+builder.Services.AddScoped<IManagerService, ManagerService>();
+builder.Services.AddScoped<IMemberService, MemberService>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
+builder.Services.AddScoped<IRefereeService, RefereeService>();
+builder.Services.AddScoped<IReportService, ReportService>();
+builder.Services.AddScoped<IScoreKoiService, ScoreKoiService>();
+builder.Services.AddScoped<IStaffService, StaffService>();
+builder.Services.AddScoped<IUserReportService, UserReportService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+
+
 
 var app = builder.Build();
 
