f997af24d30a83a90acffac412ae5717823b3d10
Datbui1711 <157027445+Datbui1711@users.noreply.github.com>
2024-11-10 00:33:14 +0700

    đạt

--- KoiShowManagement.Services/Interface/IScoreKoiService.cs
+++ KoiShowManagement.Services/Interface/IScoreKoiService.cs
@@ -1,17 +1,17 @@
-﻿using KoiShowManagement.Repositories.Entities;
 using System;
-using System.Threading.Task;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using KoiShowManagement.Repositories.Entities;
+
 namespace KoiShowManagement.Services.Interface
 {
     public interface IScoreKoiService
     {
-        Task<List<ScoreKoi>> GetAllScoreKois();
-        Task<bool> DelScoreKoi(int Id);
-        Task<bool>  DelScoreKoi(ScoreKoi scoreKoi);
-        Task<bool>  AddScoreKoi(ScoreKoi scoreKoi);
-        Task<bool>  UpdScoreKoi(ScoreKoi scoreKoi);
-        Task<ScoreKoi> GetScoreKoiId(int Id);
-        string? GetAll();
-        string? GetById(int id);
+        Task<List<ScoreKoi>> GetAllScoreKoisAsync();
+        Task<bool> DeleteScoreKoiAsync(int Id);
+        Task<bool> DeleteScoreKoiAsync(ScoreKoi scoreKoi);
+        Task<bool> AddScoreKoiAsync(ScoreKoi scoreKoi);
+        Task<bool> UpdateScoreKoiAsync(ScoreKoi scoreKoi);
+        Task<ScoreKoi> GetScoreKoiByIdAsync(int Id);
     }
 }
