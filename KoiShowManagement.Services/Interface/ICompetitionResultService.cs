dd644cec58a74fac7bde0c25f08dd7ff3e7dd16b
duocnguyen058 <dcnguyn058@gmail.com>
2024-11-09 22:05:06 +0700

    duoc

--- KoiShowManagement.Services/Interface/ICompetitionResultService.cs
+++ KoiShowManagement.Services/Interface/ICompetitionResultService.cs
@@ -5,11 +5,11 @@ namespace KoiShowManagement.Services.Interface
 {
     public interface ICompetitionResultService
     {
-        Task<List<CompetitionResult>> GetAllCompetitionResults();
-        Boolean DelCompetitionResult(int Id);
-        Boolean DelCompetitionResult(CompetitionResult result);
-        Boolean AddCompetitionResult(CompetitionResult result);
-        Boolean UpdCompetitionResult(CompetitionResult result);
-        Task<CompetitionResult> GetCompetitionResultById(int Id);
+        Task<List<CompetitionResult>> GetAllCompetitionResultsAsync();
+        Task<bool> DelCompetitionResultAsync(int Id);
+        Task<bool> DelCompetitionResultAsync(CompetitionResult result);
+        Task<bool> AddCompetitionResultAsync(CompetitionResult result);
+        Task<bool> UpdCompetitionResultAsync(CompetitionResult result);
+        Task<CompetitionResult> GetCompetitionResultByIdAsync(int Id);
     }
 }
--- KoiShowManagement.Services/Service/CompetitionResultService.cs
+++ KoiShowManagement.Services/Service/CompetitionResultService.cs
@@ -13,34 +13,63 @@ namespace KoiShowManagement.Services.Service
             _repository = repository;
         }
 
-        public bool AddCompetitionResult(CompetitionResult result)
+        public async Task<bool> AddCompetitionResultAsync(CompetitionResult result)
         {
-            return _repository.AddCompetitionResult(result);
+            if (result == null)
+                throw new ArgumentNullException(nameof(result), "Kết quả thi đấu không được để trống.");
+            if (result.ResultId <= 0)
+                throw new ArgumentNullException("Mã kết quả cuộc thi phải là số nguyên dương.",nameof(result.ResultId));
+            if (result.ResultId <= 0)
+                throw new ArgumentNullException("Mã kết quả cuộc thi phải là số nguyên dương.", nameof(result.ResultId));
+            if (string.IsNullOrWhiteSpace(result.Feedback))
+                throw new ArgumentNullException("Ghi nhận xét không được để hoặc chỉ chứa khoảng trống.",nameof(result.Feedback));
+            return await _repository.AddCompetitionResultAsync(result);
         }
 
-        public bool DelCompetitionResult(int Id)
+        public async Task<bool> DelCompetitionResultAsync(int Id)
         {
-            return _repository.DelCompetitionResult(Id);
+            if (Id <= 0)
+            {
+                throw new ArgumentException("ID không hợp lệ chỉ chấp nhận số nguyên dương", nameof(Id));
+            }
+            return await _repository.DelCompetitionResultAsync(Id);
         }
 
-        public bool DelCompetitionResult(CompetitionResult result)
+        public async Task<bool> DelCompetitionResultAsync(CompetitionResult result)
         {
-            return _repository.DelCompetitionResult(result);
+            if(result == null)
+            {
+                throw new ArgumentNullException(nameof(result), "Kết quả thi đấu không được để trống");
+            }
+            return await _repository.DelCompetitionResultAsync(result);
         }
 
-        public Task<List<CompetitionResult>> GetAllCompetitionResults()
+        public async Task<List<CompetitionResult>> GetAllCompetitionResultsAsync()
         {
-            return _repository.GetAllCompetitionResults();
+            return await _repository.GetAllCompetitionResultsAsync();
         }
 
-        public Task<CompetitionResult> GetCompetitionResultById(int Id)
+        public async Task<CompetitionResult> GetCompetitionResultByIdAsync(int Id)
         {
-            return _repository.GetCompetitionResultById(Id);
+            if(Id <= 0)
+            {
+                throw new ArgumentException("ID không hợp lệ chỉ chấp nhận số nguyên dương", nameof(Id));
+            }
+            return await _repository.GetCompetitionResultByIdAsync(Id);
         }
 
-        public bool UpdCompetitionResult(CompetitionResult result)
+        public async Task<bool> UpdCompetitionResultAsync(CompetitionResult result)
         {
-            return _repository.UpdCompetitionResult(result);
+            if (result == null)
+                throw new ArgumentNullException(nameof(result), "Kết quả thi đấu không được để trống.");
+            if (result.ResultId <= 0)
+                throw new ArgumentNullException("Mã kết quả cuộc thi phải là số nguyên dương.", nameof(result.ResultId));
+            if (result.ResultId <= 0)
+                throw new ArgumentNullException("Mã kết quả cuộc thi phải là số nguyên dương.", nameof(result.ResultId));
+            if (string.IsNullOrWhiteSpace(result.Feedback))
+                throw new ArgumentNullException("Ghi nhận xét không được để hoặc chỉ chứa khoảng trống.", nameof(result.Feedback));
+            return await _repository.UpdCompetitionResultAsync(result);
         }
     }
 }
+        
