4668fedf89abe7a359d9ab65ef9cf3dfad268ce5
HauDTg <tronghau105@gmail.com>
2024-11-10 14:45:21 +0700

    hau

--- KoiShowManagement.Services/Interface/IUserService.cs
+++ KoiShowManagement.Services/Interface/IUserService.cs
@@ -6,11 +6,13 @@ namespace KoiShowManagement.Services.Interface
 {
     public interface IUserService
     {
-        Task<List<User>> GetAllUsers();
-        Task<bool> DelUser(int Id);
-        Task<bool> DelUser(User user);
-        Task<bool> AddUser(User user);
-        Task<bool> UpdUser(User user);
-        Task<User> GetUserById(int Id);
+        Task<List<User>> GetAllUsersAsync();  // Async method to get all users
+        Task<User> GetUserByIdAsync(int id);  // Async method to get user by ID
+        Task<bool> AddUserAsync(User user);  // Async method to add a user
+        Task<bool> UpdUserAsync(User user);  // Async method to update a user
+        Task<bool> DelUserAsync(int id);  // Async method to delete user by ID
+        Task<bool> DelUserAsync(User user);  // Async method to delete user by object
+        Task<bool> LoginAsync(User user);  // Async method for login
+        Task<bool> RegisterAsync(User user);  // Register should be async
     }
 }
