a5fac7c5fa25dc1a6686ad908ebafa5ce07c424c
thangsvj <luongthangnguyen173@gmail.com>
2024-11-10 11:46:30 +0700

    thang

--- KoiShowManagement.Repositories/Repository/RefereeRepository.cs
+++ KoiShowManagement.Repositories/Repository/RefereeRepository.cs
@@ -1,92 +1,108 @@
-﻿using KoiShowManagement.Repositories.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KoiShowManagement.Repositories.Entities;
 using KoiShowManagement.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
-using System;
 
-namespace KoiShowManagement.Repositories.Repository
+namespace KoiShowManagementSystem.Repositories.Repository
 {
     public class RefereeRepository : IRefereeRepository
     {
         private readonly KoiShowManagementDbContext _dbContext;
+
         public RefereeRepository(KoiShowManagementDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public bool AddReferee(Referee referee)
+        public async Task<List<Referee>> GetAllRefereesAsync()
+        {
+            return await _dbContext.Referees.Include(r => r.CompetitionResults).Include(r => r.ScoreKois).ToListAsync();
+        }
+
+        public async Task<Referee> GetRefereeByIdAsync(int refereeId)
+        {
+            return await _dbContext.Referees.Include(r => r.CompetitionResults).Include(r => r.ScoreKois)
+                                            .FirstOrDefaultAsync(r => r.RefereeId == refereeId);
+        }
+
+        public async Task<bool> AddRefereeAsync(Referee referee)
         {
             try
             {
-                _dbContext.Referees.Add(referee);
-                _dbContext.SaveChanges();
+                await _dbContext.Referees.AddAsync(referee);
+                await _dbContext.SaveChangesAsync();
                 return true;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                Console.WriteLine(ex.Message);
                 return false;
             }
         }
 
-        public bool DelReferee(int Id)
-        {
-
-            try
-            {
-                var refereeToDelete = _dbContext.Referees.Where(r => r.RefereeId.Equals(Id)).FirstOrDefault();
-                if (refereeToDelete != null)
-                {
-                    _dbContext.Referees.Remove(refereeToDelete);
-                    _dbContext.SaveChanges();
-                    return true;
-                }
-                return false;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
-        }
-
-        public bool DelReferee(Referee referee)
-        {
-            try
-            {
-                _dbContext.Referees.Remove(referee);
-                _dbContext.SaveChanges();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
-        }
-
-        public async Task<List<Referee>> GetAllReferees()
-        {
-            return await _dbContext.Referees.ToListAsync();
-        }
-
-        public async Task<Referee> GetByRefereeId(int Id)
-        {
-            return await _dbContext.Referees.Where(r => r.RefereeId.Equals(Id)).FirstOrDefaultAsync();
-        }
-
-        public bool UpdReferee(Referee referee)
+        public async Task<bool> UpdateRefereeAsync(Referee referee)
         {
             try
             {
                 _dbContext.Referees.Update(referee);
-                _dbContext.SaveChanges();
+                await _dbContext.SaveChangesAsync();
                 return true;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                Console.WriteLine(ex.Message);
                 return false;
             }
+        }
+
+        public async Task<bool> DeleteRefereeByIdAsync(int refereeId)
+        {
+            var referee = await GetRefereeByIdAsync(refereeId);
+            if (referee == null) return false;
+            return await DeleteRefereeAsync(referee);
+        }
+
+        public async Task<bool> DeleteRefereeAsync(Referee referee)
+        {
+            try
+            {
+                _dbContext.Referees.Remove(referee);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<Referee>> GetRefereesByExpertiseLevelAsync(string expertiseLevel)
+        {
+            return await _dbContext.Referees.Where(r => r.ExpertiseLevel == expertiseLevel).ToListAsync();
+        }
+
+        // Triển khai chức năng tìm kiếm với các tiêu chí tùy chọn
+        public async Task<List<Referee>> SearchRefereesAsync(string name = null, string email = null, string expertiseLevel = null)
+        {
+            var query = _dbContext.Referees.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                query = query.Where(r => r.Name.Contains(name));
+            }
+
+            if (!string.IsNullOrWhiteSpace(email))
+            {
+                query = query.Where(r => r.Email.Contains(email));
+            }
+
+            if (!string.IsNullOrWhiteSpace(expertiseLevel))
+            {
+                query = query.Where(r => r.ExpertiseLevel.Contains(expertiseLevel));
+            }
+
+            return await query.Include(r => r.CompetitionResults).Include(r => r.ScoreKois).ToListAsync();
         }
     }
 }
