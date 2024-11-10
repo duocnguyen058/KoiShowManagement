a1b0c3894aa216299ca21e4ad240d402557f279d
thangsvj <luongthangnguyen173@gmail.com>
2024-11-10 11:44:35 +0700

    thang

--- KoiShowManagement.Repositories/Interface/IRefereeRepository.cs
+++ KoiShowManagement.Repositories/Interface/IRefereeRepository.cs
@@ -1,17 +1,20 @@
-﻿using KoiShowManagement.Repositories.Entities;
-using System;
-
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KoiShowManagement.Repositories.Entities;
 
 namespace KoiShowManagement.Repositories.Interface
 {
     public interface IRefereeRepository
     {
-        Task<List<Referee>> GetAllReferees();
-        Boolean DelReferee(int Id);
-        Boolean DelReferee(Referee referee);
-        Boolean AddReferee(Referee referee);
-        Boolean UpdReferee(Referee referee);
-        Task<Referee> GetByRefereeId(int Id);
+        Task<List<Referee>> GetAllRefereesAsync();
+        Task<Referee> GetRefereeByIdAsync(int refereeId);
+        Task<bool> AddRefereeAsync(Referee referee);
+        Task<bool> UpdateRefereeAsync(Referee referee);
+        Task<bool> DeleteRefereeByIdAsync(int refereeId);
+        Task<bool> DeleteRefereeAsync(Referee referee);
+        Task<List<Referee>> GetRefereesByExpertiseLevelAsync(string expertiseLevel);
 
+        // Phương thức tìm kiếm với các tiêu chí tùy chọn
+        Task<List<Referee>> SearchRefereesAsync(string name = null, string email = null, string expertiseLevel = null);
     }
 }
