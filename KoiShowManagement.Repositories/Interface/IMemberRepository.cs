0e52c9b44884ceb79d4453274a7cd254d3438ab2
nguyenletuankiet205 <nguyenletuankiet.qng@gmail.com>
2024-11-10 12:20:49 +0700

    kiet

--- KoiShowManagement.Repositories/Interface/IMemberRepository.cs
+++ KoiShowManagement.Repositories/Interface/IMemberRepository.cs
@@ -12,5 +12,8 @@ namespace KoiShowManagement.Repositories.Interface
         Task<bool> UpdateMemberAsync(Member member);
         Task<bool> DeleteMemberAsync(int memberId);
         Task<bool> DeleteMemberAsync(Member member);
+
+        // Thêm phương thức tìm kiếm
+        Task<List<Member>> SearchMembersAsync(string? name, string? email, string? membershipType);
     }
 }
