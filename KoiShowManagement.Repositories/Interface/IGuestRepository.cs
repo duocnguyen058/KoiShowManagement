59e486a27ab3b9dd55d1da146c167eef0ffce104
Datbui1711 <157027445+Datbui1711@users.noreply.github.com>
2024-11-10 12:04:01 +0700

    đạt

--- KoiShowManagement.Repositories/Interface/IGuestRepository.cs
+++ KoiShowManagement.Repositories/Interface/IGuestRepository.cs
@@ -1,15 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using KoiShowManagement.Repositories.Entities;
-
 
 namespace KoiShowManagement.Repositories.Interface
 {
     public interface IGuestRepository
     {
-        Task<List<Guest>> GetGuestsAsync();
-        Task<bool> DelGuestAsync(int Id);
-        Task<bool> DelGuestAsync(Guest guest);
-        Task<bool> AddGuestAsync(Guest guest);
-        Task<bool> UpdGuestAsync(Guest guest);
+        Task<List<Guest>> GetAllGuestsAsync();
         Task<Guest> GetGuestByIdAsync(int Id);
+        Task<bool> AddGuestAsync(Guest guest);
+        Task<bool> UpdateGuestAsync(Guest guest);
+        Task<bool> DeleteGuestAsync(int Id);
+        Task<bool> DeleteGuestAsync(Guest guest);
+
     }
 }
