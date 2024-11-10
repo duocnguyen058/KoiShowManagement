fb98370da8d4b9f6433fd501e579ac2a232cf090
HauDTg <tronghau105@gmail.com>
2024-11-10 12:48:20 +0700

    hau

--- KoiShowManagement.Repositories/Interface/IReportRepository.cs
+++ KoiShowManagement.Repositories/Interface/IReportRepository.cs
@@ -1,15 +1,16 @@
-using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using KoiShowManagement.Repositories.Entities;
 
 namespace KoiShowManagement.Repositories.Interface
 {
     public interface IReportRepository
     {
-        Task<List<Report>> GetAllReports();
-        Boolean DelReport(int Id);
-        Boolean DelReport(Report report);
-        Boolean AddReport(Report report);
-        Boolean UpdReport(Report report);
-        Task<Report> GetReportById(int Id);
+        Task<List<Report>> GetAllReportsAsync();
+        Task<bool> DelReportAsync(int Id);
+        Task<bool> DelReportAsync(Report report);
+        Task<bool> AddReportAsync(Report report);
+        Task<bool> UpdReportAsync(Report report);
+        Task<Report> GetReportByIdAsync(int Id);
     }
 }
