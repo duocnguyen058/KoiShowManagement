0516f19609ab196bfd17571e8c4f8a631550ec1a
Datbui1711 <157027445+Datbui1711@users.noreply.github.com>
2024-11-10 14:50:11 +0700

    đạt

--- KoiShowManagement.Services/Interface/IKoiService.cs
+++ KoiShowManagement.Services/Interface/IKoiService.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using KoiShowManagement.Repositories.Entities;
@@ -7,11 +6,11 @@ namespace KoiShowManagement.Services.Interface
 {
     public interface IKoiService
     {
-        Task<List<Koi>> GetAllKoisAsync();
-        Task<bool> DeleteKoiAsync(int Id);
-        Task<bool> DeleteKoiAsync(Koi koi);
-        Task<bool> AddKoiAsync(Koi koi);
-        Task<bool> UpdateKoiAsync(Koi koi);
-        Task<Koi> GetKoiByIdAsync(int Id);
+        Task<List<Koi>> GetAllKoisAsync();  // Fetch all Kois asynchronously
+        Task<Koi> GetKoiByIdAsync(int id);  // Fetch a single Koi by ID asynchronously
+        Task<bool> AddKoiAsync(Koi koi);    // Add a new Koi asynchronously
+        Task<bool> UpdateKoiAsync(Koi koi); // Update an existing Koi asynchronously
+        Task<bool> DeleteKoiAsync(int id);  // Delete a Koi by ID asynchronously
+        Task<bool> DeleteKoiAsync(Koi koi); // Delete a Koi by object asynchronously
     }
 }
