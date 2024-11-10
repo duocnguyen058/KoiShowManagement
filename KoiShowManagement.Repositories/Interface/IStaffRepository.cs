f6565e53697f771b2dec6ff1808af47f21a4f320
nguyenletuankiet205 <nguyenletuankiet.qng@gmail.com>
2024-11-10 12:23:15 +0700

    kiet

--- KoiShowManagement.Repositories/Interface/IStaffRepository.cs
+++ KoiShowManagement.Repositories/Interface/IStaffRepository.cs
@@ -12,5 +12,8 @@ namespace KoiShowManagement.Repositories.Interface
         Task<bool> UpdateStaffAsync(Staff staff);
         Task<bool> DeleteStaffAsync(int staffId);
         Task<bool> DeleteStaffAsync(Staff staff);
+
+        // Thêm phương thức tìm kiếm
+        Task<List<Staff>> SearchStaffAsync(string? name, string? email, string? position);
     }
 }
