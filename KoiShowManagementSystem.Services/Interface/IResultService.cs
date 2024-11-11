e3e1292d918b2180feff4dacb91a5665723449c8
thangsvj <luongthangnguyen173@gmail.com>
2024-11-11 22:54:44 +0700

    thang

---
+++ KoiShowManagementSystem.Services/Interface/IResultService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KoiShowManagementSystem.Repositories.Entities;
+
+namespace KoiShowManagementSystem.Services.Interface
+{
+    public interface IResultService
+    {
+        Task<bool> CreateResultAsync(Result result);
+        Task<bool> UpdateResultAsync(Result result);
+        Task<bool> DeleteResultAsync(int resultId);
+        Task<Result> GetResultByIdAsync(int resultId);
+        Task<IEnumerable<Result>> GetAllResultsAsync();
+        Task<IEnumerable<Result>> GetResultsForCompetitionAsync(int competitionId);
+    }
+}
