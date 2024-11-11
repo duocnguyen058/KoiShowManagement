7078c924fb118183a5bd491bb3940405a1eb6517
Datbt <datbuithanh1711@gmail.com>
2024-11-11 21:40:34 +0700

    đạt

---
+++ KoiShowManagementSystem.Services/Interface/ICompetitionCategoryService.cs
@@ -0,0 +1,19 @@
+﻿using KoiShowManagementSystem.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiShowManagementSystem.Services.Interface
+{
+    public interface ICompetitionCategoryService
+    {
+        Task<List<CompetitionCategory>> GetAllCompetitionCategoriesAsync();
+        Task<CompetitionCategory> GetCompetitionCategoryByIdAsync(int Id);
+        Task<bool> AddCompetitionCategoryAsync(CompetitionCategory category);
+        Task<bool> UpdateCompetitionCategoryAsync(CompetitionCategory category);
+        Task<bool> DeleteCompetitionCategoryAsync(int Id);
+        Task<bool> DeleteCompetitionCategoryAsync(CompetitionCategory category);
+    }
+}
