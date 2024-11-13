18d7e1d5ab26767c12318f41254f30206a94b6c3
khanhlazy <khanhvps.mmo@gmail.com>
2024-11-13 13:52:26 +0700

    khánh

--- KoiShowManagementSystem.Repositories/Entities/KoiShowManagementDbcontextContext.cs
+++ KoiShowManagementSystem.Repositories/Entities/KoiShowManagementDbcontextContext.cs
@@ -38,19 +38,9 @@ public partial class KoiShowManagementDbcontextContext : DbContext
     public virtual DbSet<Score> Scores { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-<<<<<<< HEAD
-    {
-#warning To protect potentially sensitive information in your connection string, use configuration settings.
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=KoiShowManagementDBContext;Persist Security Info=True;User ID=sa;Password=Khanhvps18@;MultipleActiveResultSets=True;TrustServerCertificate=True");
-        }
-    }
-=======
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=KoiShowManagementDBContext;Persist Security Info=True;User ID=SA;Password=123456aA@$;MultipleActiveResultSets=True;TrustServerCertificate=True");
 
->>>>>>> 326395af6a86d8090aa86279419b2334c8bcafb1
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
