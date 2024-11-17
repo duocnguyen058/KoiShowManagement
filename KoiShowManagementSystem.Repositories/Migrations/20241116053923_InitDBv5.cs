f2a6309de5deb1bdec28e266176d650d38be6dbb
HauDTg <tronghau105@gmail.com>
2024-11-18 00:00:39 +0700

    hau

--- KoiShowManagementSystem.Repositories/Migrations/20241116053923_InitDBv5.cs
+++ KoiShowManagementSystem.Repositories/Migrations/20241116053923_InitDBv5.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,19 +10,21 @@ namespace KoiShowManagementSystem.Repositories.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Thêm cột "PhotoPath" vào bảng "Kois"
             migrationBuilder.AddColumn<string>(
-                name: "PhotoPath",
-                table: "Kois",
-                type: "nvarchar(max)",
-                nullable: true);
+                name: "PhotoPath",  // Tên cột cần thêm
+                table: "Kois",      // Tên bảng
+                type: "nvarchar(max)",  // Kiểu dữ liệu của cột
+                nullable: true);    // Cột có thể null
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            // Xóa cột "PhotoPath" khỏi bảng "Kois" nếu rollback migration
             migrationBuilder.DropColumn(
-                name: "PhotoPath",
-                table: "Kois");
+                name: "PhotoPath",  // Tên cột cần xóa
+                table: "Kois");     // Tên bảng
         }
     }
 }
