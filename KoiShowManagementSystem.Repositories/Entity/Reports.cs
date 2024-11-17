08c77162f9600b7c8b77ff0b4967cde6094ad643
nguyenletuankiet205 <nguyenletuankiet.qng@gmail.com>
2024-11-17 21:04:51 +0700

    kiet

--- KoiShowManagementSystem.Repositories/Entity/Reports.cs
+++ KoiShowManagementSystem.Repositories/Entity/Reports.cs
@@ -1,15 +1,19 @@
-﻿namespace KoiShowManagementSystem.Repositories.Entity
+namespace KoiShowManagementSystem.Repositories.Entity
 {
+    // Lớp đại diện cho các báo cáo liên quan đến sự kiện.
     public class Reports
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        public int EventsId { get; set; }
-        public Events Events { get; set; }
+        public int Id { get; set; } // ID của báo cáo, tự động tăng.
+
+        public int EventsId { get; set; } // ID của sự kiện liên quan đến báo cáo.
+        public Events Events { get; set; } // Quan hệ với đối tượng Events, thể hiện sự kiện mà báo cáo thuộc về.
+
         [Column(TypeName = "nvarchar(100)")]
-        public string ReportType { get; set; }
+        public string ReportType { get; set; } // Loại báo cáo (ví dụ: Báo cáo kết quả, Báo cáo thống kê,...).
+
         [Column(TypeName = "nvarchar(100)")]
-        public string ReportData { get; set; }
+        public string ReportData { get; set; } // Dữ liệu báo cáo, thông tin chi tiết về báo cáo đó.
     }
 }
