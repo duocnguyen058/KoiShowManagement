a6735ebdb95d26e01711861e13269426ab160ba0
HauDTg <tronghau105@gmail.com>
2024-11-17 23:54:52 +0700

    hau

--- KoiShowManagementSystem.Repositories/Migrations/20241115032801_InitDBv3.cs
+++ KoiShowManagementSystem.Repositories/Migrations/20241115032801_InitDBv3.cs
@@ -1,22 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace KoiShowManagementSystem.Repositories.Migrations
 {
     /// <inheritdoc />
+    // Lớp migration 'InitDBv3' được sử dụng để thực hiện hoặc hủy bỏ các thay đổi trong cơ sở dữ liệu.
     public partial class InitDBv3 : Migration
     {
         /// <inheritdoc />
+        // Phương thức 'Up' thực hiện các thay đổi trong cơ sở dữ liệu khi áp dụng migration.
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            // Chưa có thay đổi nào trong migration này.
         }
 
         /// <inheritdoc />
+        // Phương thức 'Down' hủy bỏ các thay đổi trong cơ sở dữ liệu khi rollback migration.
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            // Chưa có thay đổi nào trong migration này.
         }
     }
 }
