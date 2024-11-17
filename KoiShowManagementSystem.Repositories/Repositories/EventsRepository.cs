b490c312d9100bcca0f750a48f0cdcb73a638ddf
nguyenletuankiet205 <nguyenletuankiet.qng@gmail.com>
2024-11-17 16:48:15 +0700

    kiet

--- KoiShowManagementSystem.Repositories/Repositories/EventsRepository.cs
+++ KoiShowManagementSystem.Repositories/Repositories/EventsRepository.cs
@@ -1,1 +1,18 @@
+namespace KoiShowManagementSystem.Repositories.Repositories
+{
+    public interface IEventsRepository : IRepository<Events>
+    {
+        List<Events> GetEvents();
+    }
+    public class EventsRepository : RepositoryBase<Events>, IEventsRepository
+    {
+        public EventsRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
 
+        public List<Events> GetEvents()
+        {
+            return _dbContext.Events.ToList();
+        }
+    }
+}
