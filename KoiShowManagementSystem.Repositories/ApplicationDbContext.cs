c9beb7202c78db3cbc5f8b6e8f86aef4447c40de
nguyenletuankiet205 <nguyenletuankiet.qng@gmail.com>
2024-11-17 21:06:56 +0700

    kiet

--- KoiShowManagementSystem.Repositories/ApplicationDbContext.cs
+++ KoiShowManagementSystem.Repositories/ApplicationDbContext.cs
@@ -1,18 +1,21 @@
-﻿namespace KoiShowManagementSystem.Repositories
+namespace KoiShowManagementSystem.Repositories
 {
+    // Lớp đại diện cho ngữ cảnh cơ sở dữ liệu của ứng dụng, chứa các bảng (DbSet) trong cơ sở dữ liệu.
     public class ApplicationDbContext : DbContext
     {
-        
+        // Constructor nhận đối số options để cấu hình kết nối cơ sở dữ liệu.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+            : base(options)
         {
         }
-        public DbSet<Users> Users { get; set; }
-        public DbSet<Scores> Scores { get; set; }
-        public DbSet<Reports> Reports { get; set; }
-        public DbSet<Koi> Kois { get; set; }
-        public DbSet<JudgeAssignments> JudgeAssignments { get; set; }
-        public DbSet<Events> Events { get; set; }
-        public DbSet<Event_Koi_Participation> Event_Koi_Participations { get; set; }
+
+        // Các DbSet đại diện cho các bảng trong cơ sở dữ liệu.
+        public DbSet<Users> Users { get; set; } // Bảng Users, lưu trữ thông tin người dùng.
+        public DbSet<Scores> Scores { get; set; } // Bảng Scores, lưu trữ điểm số của Koi trong sự kiện.
+        public DbSet<Reports> Reports { get; set; } // Bảng Reports, lưu trữ thông tin báo cáo của sự kiện.
+        public DbSet<Koi> Kois { get; set; } // Bảng Kois, lưu trữ thông tin về Koi.
+        public DbSet<JudgeAssignments> JudgeAssignments { get; set; } // Bảng JudgeAssignments, lưu trữ thông tin phân công giám khảo.
+        public DbSet<Events> Events { get; set; } // Bảng Events, lưu trữ thông tin sự kiện.
+        public DbSet<Event_Koi_Participation> Event_Koi_Participations { get; set; } // Bảng Event_Koi_Participations, lưu trữ thông tin tham gia sự kiện của Koi.
     }
 }
