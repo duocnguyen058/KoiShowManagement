d1b50b12624b14959368d2da04cc9704c6fba3c3
HauDTg <tronghau105@gmail.com>
2024-11-17 17:00:25 +0700

    hau

--- KoiShowManagementSystem.Repositories/Migrations/20241115032801_InitDBv3.cs
+++ KoiShowManagementSystem.Repositories/Migrations/20241115032801_InitDBv3.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KoiShowManagementSystem.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDBv3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
